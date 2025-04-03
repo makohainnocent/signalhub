1626f0b661443516e2222cda553fb1fa79f2b5f2
makoha innocent <vantagepoint2019@gmail.com>
2025-04-04 01:57:06 +0300

    migration

--- Api/Core/Extensions/Extensions.cs
+++ Api/Core/Extensions/Extensions.cs
@@ -38,7 +38,7 @@ namespace Api.Core.Extensions
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Animal Tracking API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SignalHub API", Version = "v1" });
 
                 // Configure Swagger to use the Authorization header
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
